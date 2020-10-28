1a75a54126c2ab2c55c7ed75ae0d9320e798c09c
hooray <304327508@qq.com>
2020-10-28 10:08:30 +0800

    优化layout布局页面相关组件文件存放目录

---
+++ src/layout/components/Breadcrumb/index.vue
@@ -0,0 +1,110 @@
+<template>
+    <div class="breadcrumb-container">
+        <div class="left-box">
+            <div v-if="$store.state.global.enableSidebarCollapse" :class="{
+                'sidebar-collapse': true,
+                'is-collapse': $store.state.global.sidebarCollapse
+            }" @click="$store.commit('global/toggleSidebarCollapse')"
+            >
+                <svg-icon name="collapse" />
+            </div>
+            <el-breadcrumb separator-class="el-icon-arrow-right">
+                <transition-group name="breadcrumb">
+                    <el-breadcrumb-item v-for="item in breadcrumbList" :key="item.path" :to="item.path">{{ item.meta.title }}</el-breadcrumb-item>
+                </transition-group>
+            </el-breadcrumb>
+        </div>
+        <UserMenu />
+    </div>
+</template>
+
+<script>
+import UserMenu from '../UserMenu'
+
+export default {
+    name: 'Breadcrumb',
+    components: {
+        UserMenu
+    },
+    computed: {
+        breadcrumbList() {
+            let matched = this.$route.matched.filter(item => item.meta && item.meta.title && item.meta.breadcrumb !== false)
+            if (matched.length != 0) {
+                if (this.$store.state.global.enableDashboard) {
+                    if (!(matched[0].name == 'dashboard' && matched[0].path == '/dashboard')) {
+                        matched = [{ path: '/dashboard', meta: { title: this.$store.state.global.dashboardTitle }}].concat(matched)
+                    }
+                }
+            }
+            return matched
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.breadcrumb-container {
+    position: fixed;
+    z-index: 999;
+    top: 0;
+    right: 0;
+    left: 50%;
+    width: calc(100% - #{$g_sidebar_width});
+    transform: translateX(calc(-50% + #{$g_sidebar_width} * 0.5));
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 20px;
+    height: $g-breadcrumb-height;
+    background-color: #fff;
+    transition: 0.3s;
+    box-shadow: 0 0 1px 0 #ccc;
+    &.shadow {
+        box-shadow: 0 10px 10px -10px #ccc;
+    }
+    .left-box {
+        display: flex;
+        align-items: center;
+        .sidebar-collapse {
+            display: flex;
+            align-items: center;
+            margin-left: -20px;
+            padding: 0 20px;
+            height: 50px;
+            cursor: pointer;
+            transition: 0.3s;
+            &:hover {
+                background-image: linear-gradient(to right, #ddd, transparent);
+            }
+            .svg-icon {
+                transition: 0.3s;
+            }
+            &.is-collapse .svg-icon {
+                transform: rotateZ(-180deg);
+            }
+        }
+    }
+    ::v-deep .el-breadcrumb {
+        .el-breadcrumb__item {
+            span {
+                font-weight: normal;
+            }
+            &:last-child span {
+                color: #97a8be;
+            }
+        }
+    }
+}
+// 面包屑动画
+.breadcrumb-enter-active {
+    transition: all 0.3s;
+}
+.breadcrumb-enter,
+.breadcrumb-leave-active {
+    opacity: 0;
+    transform: translateX(20px);
+}
+.breadcrumb-leave-active {
+    position: absolute;
+}
+</style>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -64,27 +64,7 @@
                     </div>
                 </div>
                 <div class="main-container">
-                    <div :class="{
-                        'breadcrumb-container': true,
-                        'shadow': scrollTop
-                    }"
-                    >
-                        <div class="left-box">
-                            <div v-if="$store.state.global.enableSidebarCollapse" :class="{
-                                'sidebar-collapse': true,
-                                'is-collapse': $store.state.global.sidebarCollapse
-                            }" @click="$store.commit('global/toggleSidebarCollapse')"
-                            >
-                                <svg-icon name="collapse" />
-                            </div>
-                            <el-breadcrumb separator-class="el-icon-arrow-right">
-                                <transition-group name="breadcrumb">
-                                    <el-breadcrumb-item v-for="item in breadcrumbList" :key="item.path" :to="item.path">{{ item.meta.title }}</el-breadcrumb-item>
-                                </transition-group>
-                            </el-breadcrumb>
-                        </div>
-                        <UserMenu />
-                    </div>
+                    <Breadcrumb :class="{'shadow': scrollTop}" />
                     <div class="main">
                         <transition name="main" mode="out-in">
                             <keep-alive :include="$store.state.keepAlive.list">
@@ -104,9 +84,18 @@
 
 <script>
 import variables from '@/assets/styles/resources/variables.scss'
+import Logo from './components/Logo'
+import UserMenu from './components/UserMenu'
+import SidebarItem from './components/SidebarItem'
+import Breadcrumb from './components/Breadcrumb'
+import SearchSidebar from './components/SearchSidebar'
+import ThemeSetting from './components/ThemeSetting'
 
 export default {
     name: 'Layout',
+    components: {
+        Logo, SearchSidebar, SidebarItem, Breadcrumb, ThemeSetting, UserMenu
+    },
     inject: ['reload'],
     data() {
         return {
@@ -130,17 +119,6 @@ export default {
                 realSidebarWidth += parseInt(variables.g_sub_sidebar_width)
             }
             return `${realSidebarWidth}px`
-        },
-        breadcrumbList() {
-            let matched = this.$route.matched.filter(item => item.meta && item.meta.title && item.meta.breadcrumb !== false)
-            if (matched.length != 0) {
-                if (this.$store.state.global.enableDashboard) {
-                    if (!(matched[0].name == 'dashboard' && matched[0].path == '/dashboard')) {
-                        matched = [{ path: '/dashboard', meta: { title: this.$store.state.global.dashboardTitle }}].concat(matched)
-                    }
-                }
-            }
-            return matched
         }
     },
     mounted() {
@@ -381,58 +359,6 @@ header {
         background-color: #f5f7f9;
         box-shadow: 1px 0 0 0 darken($g-main-bg, 10);
         transition: 0.3s;
-        .breadcrumb-container {
-            position: fixed;
-            z-index: 999;
-            top: 0;
-            right: 0;
-            left: 50%;
-            width: calc(100% - #{$g_sidebar_width});
-            transform: translateX(calc(-50% + #{$g_sidebar_width} * 0.5));
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            padding: 0 20px;
-            height: $g-breadcrumb-height;
-            background-color: #fff;
-            transition: 0.3s;
-            box-shadow: 0 0 1px 0 #ccc;
-            &.shadow {
-                box-shadow: 0 10px 10px -10px #ccc;
-            }
-            .left-box {
-                display: flex;
-                align-items: center;
-                .sidebar-collapse {
-                    display: flex;
-                    align-items: center;
-                    margin-left: -20px;
-                    padding: 0 20px;
-                    height: 50px;
-                    cursor: pointer;
-                    transition: 0.3s;
-                    &:hover {
-                        background-image: linear-gradient(to right, #ddd, transparent);
-                    }
-                    .svg-icon {
-                        transition: 0.3s;
-                    }
-                    &.is-collapse .svg-icon {
-                        transform: rotateZ(-180deg);
-                    }
-                }
-            }
-            ::v-deep .el-breadcrumb {
-                .el-breadcrumb__item {
-                    span {
-                        font-weight: normal;
-                    }
-                    &:last-child span {
-                        color: #97a8be;
-                    }
-                }
-            }
-        }
         .main {
             height: 100%;
             flex: auto;
@@ -473,19 +399,6 @@ header + .wrapper {
     transform: translateY(20px);
 }
 .sidebar-leave-active {
-    position: absolute;
-}
-
-// 面包屑动画
-.breadcrumb-enter-active {
-    transition: all 0.3s;
-}
-.breadcrumb-enter,
-.breadcrumb-leave-active {
-    opacity: 0;
-    transform: translateX(20px);
-}
-.breadcrumb-leave-active {
     position: absolute;
 }
 
