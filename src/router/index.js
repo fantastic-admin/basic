eb31ab989f79be53ece947e96a8698bc14078ba9
hooray <304327508@qq.com>
2020-11-06 00:22:01 +0800

    优化百度统计代码

--- src/router/index.js
+++ src/router/index.js
@@ -178,7 +178,7 @@ router.beforeEach(async(to, from, next) => {
             // 百度统计代码
             if (process.env.VUE_APP_TYPE == 'example') {
                 if (window._hmt) {
-                    window._hmt.push(['_trackPageview', '/#' + to.fullPath])
+                    window._hmt.push(['_trackPageview', location.pathname + '#' + to.fullPath])
                 }
             }
             next()
@@ -195,7 +195,7 @@ router.beforeEach(async(to, from, next) => {
             // 百度统计代码
             if (process.env.VUE_APP_TYPE == 'example') {
                 if (window._hmt) {
-                    window._hmt.push(['_trackPageview', '/#' + to.fullPath])
+                    window._hmt.push(['_trackPageview', location.pathname + '#' + to.fullPath])
                 }
             }
             next()
