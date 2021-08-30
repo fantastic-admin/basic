12c75068287f633b2dd99f91381ad5b45557fde4
hooray <304327508@qq.com>
2021-08-30 10:08:23 +0800

    移除组件全局引用，改成 vite-plugin-components 的自动引入

--- vite/plugins/restart.js
+++ vite/plugins/restart.js
@@ -3,7 +3,8 @@ import ViteRestart from 'vite-plugin-restart'
 export default function createRestart() {
     return ViteRestart({
         restart: [
-            '.env.development'
+            '.env.development',
+            'vite/plugins/index.js'
         ]
     })
 }
