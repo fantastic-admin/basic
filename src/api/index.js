97122d14676b76bab7ba2446f080f1a6c749bb04
hooray <304327508@qq.com>
2021-09-09 10:34:05 +0800

    更新依赖，修改 axios baseURL设置

--- src/api/index.js
+++ src/api/index.js
@@ -14,7 +14,7 @@ const toLogin = () => {
 }
 
 const api = axios.create({
-    baseURL: import.meta.env.DEV ? '/proxy/' : import.meta.env.VITE_APP_API_BASEURL,
+    baseURL: import.meta.env.DEV && import.meta.env.VITE_OPEN_PROXY === 'true' ? '/proxy/' : import.meta.env.VITE_APP_API_BASEURL,
     timeout: 10000,
     responseType: 'json'
 })
