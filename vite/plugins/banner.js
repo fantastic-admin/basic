07ef47f649e2fd62dfe5062201b1915b4ab00f6b
hooray <304327508@qq.com>
2021-09-20 07:22:28 +0800

    修复http-server预览指令报错的问题，vite相关相关插件配置优化

--- vite/plugins/banner.js
+++ vite/plugins/banner.js
@@ -3,8 +3,10 @@ import banner from 'vite-plugin-banner'
 export default function createBanner() {
     return banner(`
 /**
- * name: Fantastic-admin
- * homepage: https://hooray.gitee.io/fantastic-admin/
+ * 由 Fantastic-admin 提供技术支持
+ * https://hooray.gitee.io/fantastic-admin/
+ * Powered by Fantastic-admin
+ * https://hooray.github.io/fantastic-admin/
  */
     `)
 }
