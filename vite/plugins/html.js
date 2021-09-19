ba74283b8a9d649b7ff78b73d92da03487d90da5
hooray <304327508@qq.com>
2021-09-20 07:28:10 +0800

    修复http-server预览指令报错的问题，vite相关相关插件配置优化

--- vite/plugins/html.js
+++ vite/plugins/html.js
@@ -1,5 +1,9 @@
 import htmlPlugin from 'vite-plugin-html'
 
+const copyright_common_style = 'font-size: 14px; margin-bottom: 2px; padding: 6px 8px; color: #fff;'
+const copyright_main_style = `${copyright_common_style} background: #e24329;`
+const copyright_sub_style = `${copyright_common_style} background: #707070;`
+
 export default function createHtml(env, isBuild) {
     const { VITE_APP_TITLE, VITE_APP_DEBUG_TOOL, VITE_APP_MODE } = env
     const html = htmlPlugin({
@@ -7,7 +11,13 @@ export default function createHtml(env, isBuild) {
             injectData: {
                 title: VITE_APP_TITLE,
                 debugTool: VITE_APP_DEBUG_TOOL,
-                appMode: VITE_APP_MODE
+                appMode: VITE_APP_MODE,
+                copyrightScript: `
+<script>
+console.info('%c由%cFantastic-admin%c提供技术支持', '${copyright_sub_style}', '${copyright_main_style}', '${copyright_sub_style}', '\\nhttps://hooray.gitee.io/fantastic-admin/');
+console.info('%cPowered by%cFantastic-admin', '${copyright_sub_style}', '${copyright_main_style}', '\\nhttps://hooray.github.io/fantastic-admin/');
+</script>
+                `
             }
         },
         minify: isBuild
--- vite/plugins/restart.js
+++ vite/plugins/restart.js
@@ -4,7 +4,16 @@ export default function createRestart() {
     return ViteRestart({
         restart: [
             '.env.development',
-            'vite/plugins/index.js'
+            'vite/plugins/index.js',
+            'vite/plugins/auto-import.js',
+            'vite/plugins/banner.js',
+            'vite/plugins/components.js',
+            'vite/plugins/compression.js',
+            'vite/plugins/html.js',
+            'vite/plugins/mock.js',
+            'vite/plugins/restart.js',
+            'vite/plugins/spritesmith.js',
+            'vite/plugins/svg-icon.js'
         ]
     })
 }
