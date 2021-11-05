842314eaced799daccda6657a18e8785999b888f
hooray <304327508@qq.com>
2021-11-05 10:54:35 +0800

    移动端下切换路由时自动收起侧边栏导航

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -55,6 +55,14 @@ watch(() => store.state.settings.sidebarCollapse, val => {
     }
 })
 
+watch(() => routeInfo.path, () => {
+    if (store.state.settings.mode === 'mobile') {
+        store.commit('settings/updateThemeSetting', {
+            sidebarCollapse: true
+        })
+    }
+})
+
 onMounted(() => {
     proxy.$hotkeys('f5', e => {
         if (store.state.settings.enablePageReload) {
