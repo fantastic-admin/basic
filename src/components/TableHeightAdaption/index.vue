c195fd2bc2616edc096050ea45e5e827fce9d82c
hooray <304327508@qq.com>
2022-01-06 01:14:39 +0800

    优化 TableHeightAdaption 组件

--- src/components/TableHeightAdaption/index.vue
+++ src/components/TableHeightAdaption/index.vue
@@ -7,9 +7,12 @@
 <script setup name="TableHeightAdaption">
 onMounted(() => {
     nextTick(() => {
-        // 获取表头高度，然后设置 .el-table__body-wrapper 的 height
+        // 获取表头高度，然后设置 .el-table__body-wrapper 和 .el-table__fixed-body-wrapper 的 height
         let height = document.getElementsByClassName('el-table__header-wrapper')[0].offsetHeight
         document.getElementsByClassName('el-table__body-wrapper')[0].style.height = `calc(100% - ${height}px)`
+        for (let item of document.getElementsByClassName('el-table__fixed-body-wrapper')) {
+            item.style.height = `calc(100% - ${height}px)`
+        }
     })
 })
 </script>
