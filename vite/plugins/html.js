6ba00f8a73f1f8b592b28ecd0ae9e608bdbfd11f
hooray <304327508@qq.com>
2022-01-28 10:32:19 +0800

    更新依赖，更新vite插件配置

--- vite/plugins/html.js
+++ vite/plugins/html.js
@@ -1,4 +1,4 @@
-import htmlPlugin from 'vite-plugin-html'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 const copyright_common_style = 'font-size: 14px; margin-bottom: 2px; padding: 6px 8px; color: #fff;'
 const copyright_main_style = `${copyright_common_style} background: #e24329;`
@@ -6,9 +6,9 @@ const copyright_sub_style = `${copyright_common_style} background: #707070;`
 
 export default function createHtml(env, isBuild) {
     const { VITE_APP_TITLE, VITE_APP_DEBUG_TOOL } = env
-    const html = htmlPlugin({
+    const html = createHtmlPlugin({
         inject: {
-            injectData: {
+            data: {
                 title: VITE_APP_TITLE,
                 debugTool: VITE_APP_DEBUG_TOOL,
                 copyrightScript: `
