64884b2c356a759cb9022669f349eeeddfd79e9e
Hooray <304327508@qq.com>
2022-05-12 11:25:27 +0800

    环境变量里增加 mock 开关，解决生产环境上传组件无法上传的问题

--- vite/plugins/mock.js
+++ vite/plugins/mock.js
@@ -1,8 +1,11 @@
 import { viteMockServe } from 'vite-plugin-mock'
 
-export default function createMock() {
+export default function createMock(env, isBuild) {
+    const { VITE_BUILD_MOCK } = env
     return viteMockServe({
         mockPath: 'src/mock',
+        localEnabled: !isBuild,
+        prodEnabled: isBuild && VITE_BUILD_MOCK === 'true',
         injectCode: `
             import { setupProdMockServer } from './mockProdServer';
             setupProdMockServer();
