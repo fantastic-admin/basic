3a196d680968eea546bce26218bfa43e8f8f365d
hooray <304327508@qq.com>
2022-09-26 00:28:19 +0800

    修复mock文件引入后缀错误

--- src/mockProdServer.ts
+++ src/mockProdServer.ts
@@ -1,9 +1,9 @@
 import { createProdMockServer } from 'vite-plugin-mock/es/createProdMockServer'
 
 const mocks: any[] = []
-const mockContext: any = import.meta.glob('./mock/*.js', { eager: true })
+const mockContext = import.meta.glob('./mock/*.ts', { eager: true })
 Object.keys(mockContext).forEach((v) => {
-  mocks.push(...mockContext[v].default)
+  mocks.push(...(mockContext[v] as any).default)
 })
 
 export function setupProdMockServer() {
