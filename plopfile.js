436f6308b0904b4a0bf42e9e340c0c9b9dec5fa9
Hooray Hu <304327508@qq.com>
2023-07-29 10:17:09 +0800

    plop 模板读取增加校验

--- plopfile.js
+++ plopfile.js
@@ -3,6 +3,6 @@ const fs = require('node:fs')
 module.exports = function (plop) {
   plop.setWelcomeMessage('请选择需要创建的模式：')
   fs.readdirSync('./plop-templates').forEach((item) => {
-    plop.setGenerator(item, require(`./plop-templates/${item}/prompt`))
+    fs.lstatSync(`./plop-templates/${item}`).isDirectory() && plop.setGenerator(item, require(`./plop-templates/${item}/prompt`))
   })
 }
