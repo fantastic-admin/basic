c59c5017a48609a88cbfcdb919ef209b6ee374be
Hooray Hu <304327508@qq.com>
2024-01-04 01:15:10 +0800

    vite-plugin-fake-server 替换 vite-plugin-mock

--- vite/plugins/mock.ts
+++ vite/plugins/mock.ts
@@ -1,14 +1,11 @@
-import { viteMockServe } from 'vite-plugin-mock'
+import { vitePluginFakeServer } from 'vite-plugin-fake-server'
 
 export default function createMock(env, isBuild) {
   const { VITE_BUILD_MOCK } = env
-  return viteMockServe({
-    mockPath: 'src/mock',
-    localEnabled: !isBuild,
-    prodEnabled: isBuild && VITE_BUILD_MOCK === 'true',
-    injectCode: `
-import { setupProdMockServer } from './mockProdServer';
-setupProdMockServer();
-    `,
+  return vitePluginFakeServer({
+    logger: !isBuild,
+    include: 'src/mock',
+    infixName: false,
+    enableProd: isBuild && VITE_BUILD_MOCK === 'true',
   })
 }
