2e68444755a8f6b5fbdaba4310e44f8eaf712dc9
Hooray Hu <304327508@qq.com>
2024-01-04 11:21:08 +0800

    样式微调

--- src/layouts/components/SubSidebar/index.vue
+++ src/layouts/components/SubSidebar/index.vue
@@ -67,7 +67,7 @@ onMounted(() => {
         </TransitionGroup>
       </div>
     </div>
-    <div v-if="settingsStore.mode === 'pc'" class="relative flex items-center p-4" :class="[settingsStore.settings.menu.subMenuCollapse ? 'justify-center' : 'justify-end']">
+    <div v-if="settingsStore.mode === 'pc'" class="relative flex items-center px-4 py-3" :class="[settingsStore.settings.menu.subMenuCollapse ? 'justify-center' : 'justify-end']">
       <span v-show="settingsStore.settings.menu.enableSubMenuCollapseButton" class="flex-center cursor-pointer rounded bg-stone-1 p-2 transition dark:bg-stone-9 hover:bg-stone-2 dark:hover:bg-stone-8" :class="{ '-rotate-z-180': settingsStore.settings.menu.subMenuCollapse }" @click="settingsStore.toggleSidebarCollapse()">
         <SvgIcon name="toolbar-collapse" />
       </span>
