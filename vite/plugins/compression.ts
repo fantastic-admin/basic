2e34d88d94925048fe148a0a80529e3bddaa5fe5
Hooray Hu <304327508@qq.com>
2024-01-14 00:08:44 +0800

    vite-plugin-compression2 替换 vite-plugin-compression

--- vite/plugins/compression.ts
+++ vite/plugins/compression.ts
@@ -1,25 +1,24 @@
-import compression from 'vite-plugin-compression'
+import { compression } from 'vite-plugin-compression2'
+import type { PluginOption } from 'vite'
 
-export default function createCompression(env) {
-  const { VITE_BUILD_COMPRESS } = env
-  const compressList = VITE_BUILD_COMPRESS.split(',')
-  const plugin: any[] = []
-  if (compressList.includes('gzip')) {
-    plugin.push(
-      compression({
-        ext: '.gz',
-        deleteOriginFile: false,
-      }),
-    )
-  }
-  if (compressList.includes('brotli')) {
-    plugin.push(
-      compression({
-        ext: '.br',
-        algorithm: 'brotliCompress',
-        deleteOriginFile: false,
-      }),
-    )
+export default function createCompression(env, isBuild) {
+  const plugin: (PluginOption | PluginOption[])[] = []
+  if (isBuild) {
+    const { VITE_BUILD_COMPRESS } = env
+    const compressList = VITE_BUILD_COMPRESS.split(',')
+    if (compressList.includes('gzip')) {
+      plugin.push(
+        compression(),
+      )
+    }
+    if (compressList.includes('brotli')) {
+      plugin.push(
+        compression({
+          exclude: [/\.(br)$/, /\.(gz)$/],
+          algorithm: 'brotliCompress',
+        }),
+      )
+    }
   }
   return plugin
 }
