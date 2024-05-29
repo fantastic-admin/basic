4c4f811f39da8e5aa7bd6cfcb2ea1170efd02e5d
Hooray Hu <304327508@qq.com>
2024-05-29 14:29:55 +0800

    chore: 修改 unocss 写法

--- src/layouts/ui-kit/HDropdownMenu.vue
+++ src/layouts/ui-kit/HDropdownMenu.vue
@@ -19,7 +19,7 @@ const myItems = computed(() => {
   <VMenu :show-triggers="['hover']" :auto-hide="false" :popper-triggers="['hover', 'click']" :delay="200" v-bind="$attrs">
     <slot />
     <template #popper>
-      <div v-for="(item, index) in myItems" :key="index" class="p-1" border-b="~ solid stone-2 dark-stone-7 last:size-0">
+      <div v-for="(item, index) in myItems" :key="index" class="b-b-(stone-2 solid) p-1 last-b-b-size-0 dark-b-b-(stone-7)">
         <button v-for="(v, i) in item" :key="i" :disabled="v.disabled" class="w-full flex cursor-pointer items-center gap-2 border-size-0 rounded-md bg-inherit px-2 py-1.5 text-sm text-dark disabled-cursor-not-allowed dark-text-white disabled-opacity-50 hover-not-disabled-bg-stone-1 dark-hover-not-disabled-bg-stone-9" @click="v.handle">
           {{ v.label }}
         </button>
