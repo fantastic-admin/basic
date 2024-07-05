87819b4b5d7a6a0f83b21423a1965090a7385aab
Hooray Hu <304327508@qq.com>
2024-07-05 15:14:39 +0800

    chore: 移除无用代码

--- src/utils/injectionKeys.ts
+++ src/utils/injectionKeys.ts
@@ -3,5 +3,3 @@ import type { InjectionKey } from 'vue'
 export function createInjectionKey<T>(key: string): InjectionKey<T> {
   return key as any
 }
-
-export const rootMenuInjectionKey = createInjectionKey<any>('rootMenu')
