fabeb0d3a29eb1d79c87752ad7708eb3c308813e
Hooray <304327508@qq.com>
2025-03-03 23:01:28 +0800

    `FaPageMain` group 作用域

--- src/ui/components/FaPageMain/index.vue
+++ src/ui/components/FaPageMain/index.vue
@@ -45,7 +45,7 @@ function handleCollaspe() {
       </slot>
     </div>
     <div
-      :class="cn('group relative h-[calc-size(auto,size)] p-5 transition-height after:(pointer-events-none absolute bottom-0 left-0 z-1 h-12 max-h-full w-full from-transparent to-[hsl(var(--card))] bg-gradient-to-b opacity-0 transition-opacity content-empty)', {
+      :class="cn('group/pagemain relative h-[calc-size(auto,size)] p-5 transition-height after:(pointer-events-none absolute bottom-0 left-0 z-1 h-12 max-h-full w-full from-transparent to-[hsl(var(--card))] bg-gradient-to-b opacity-0 transition-opacity content-empty)', {
         'overflow-hidden': collaspe,
         'after:(opacity-100)': isCollaspe,
       }, props.mainClass)" :style="{
@@ -53,7 +53,7 @@ function handleCollaspe() {
       }"
     >
       <slot />
-      <FaButton v-if="collaspe" variant="link" size="icon" class="absolute inset-b-0 inset-s-1/2 opacity-0 transition-all -translate-x-1/2 group-hover-opacity-100" :class="{ 'rotate-x-180': !isCollaspe }" @click="handleCollaspe">
+      <FaButton v-if="collaspe" variant="link" size="icon" class="absolute inset-b-0 inset-s-1/2 opacity-0 transition-all -translate-x-1/2 group-hover/pagemain-opacity-100" :class="{ 'rotate-x-180': !isCollaspe }" @click="handleCollaspe">
         <FaIcon name="i-ep:arrow-down" class="text-xl" />
       </FaButton>
     </div>
