609e77c1593b1709a4fb685dc115d61ab13008d8
Hooray <304327508@qq.com>
2025-03-13 15:52:58 +0800

    fix: 浏览器不支持 color-mix 时，框架主题不会同步至 element-plus 主题

--- src/ui/provider/index.vue
+++ src/ui/provider/index.vue
@@ -11,23 +11,23 @@ if (isSupprotColorMix) {
   document.body.style.setProperty('--el-color-primary', 'hsl(var(--primary))')
   document.body.style.setProperty('--el-color-white', 'hsl(var(--primary-foreground))')
   document.body.style.setProperty('--el-color-black', 'hsl(var(--primary-foreground))')
+  watch(() => settingsStore.currentColorScheme, (val) => {
+    if (val === 'light') {
+      for (let index = 1; index < 10; index++) {
+        document.body.style.setProperty(`--el-color-primary-light-${index}`, `color-mix(in hsl, hsl(var(--primary)), #fff ${index * 10}%)`)
+        document.body.style.setProperty(`--el-color-primary-dark-${index}`, `color-mix(in hsl, hsl(var(--primary)), #000 ${index * 10}%)`)
+      }
+    }
+    else {
+      for (let index = 1; index < 10; index++) {
+        document.body.style.setProperty(`--el-color-primary-light-${index}`, `color-mix(in hsl, hsl(var(--primary)), #000 ${index * 10}%)`)
+        document.body.style.setProperty(`--el-color-primary-dark-${index}`, `color-mix(in hsl, hsl(var(--primary)), #fff ${index * 10}%)`)
+      }
+    }
+  }, {
+    immediate: true,
+  })
 }
-watch(() => settingsStore.currentColorScheme, (val) => {
-  if (val === 'light') {
-    for (let index = 1; index < 10; index++) {
-      document.body.style.setProperty(`--el-color-primary-light-${index}`, `color-mix(in hsl, hsl(var(--primary)), #fff ${index * 10}%)`)
-      document.body.style.setProperty(`--el-color-primary-dark-${index}`, `color-mix(in hsl, hsl(var(--primary)), #000 ${index * 10}%)`)
-    }
-  }
-  else {
-    for (let index = 1; index < 10; index++) {
-      document.body.style.setProperty(`--el-color-primary-light-${index}`, `color-mix(in hsl, hsl(var(--primary)), #000 ${index * 10}%)`)
-      document.body.style.setProperty(`--el-color-primary-dark-${index}`, `color-mix(in hsl, hsl(var(--primary)), #fff ${index * 10}%)`)
-    }
-  }
-}, {
-  immediate: true,
-})
 </script>
 
 <template>
