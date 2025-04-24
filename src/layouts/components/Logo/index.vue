5dbedd24ae5ad1f423434f0ad873d06fc7aaa5f7
Hooray <304327508@qq.com>
2025-04-24 15:55:43 +0800

    修复导航logo在禁用主页时依旧显示可点击状态

--- src/layouts/components/Logo/index.vue
+++ src/layouts/components/Logo/index.vue
@@ -26,7 +26,7 @@ const to = computed(() => settingsStore.settings.home.enable ? settingsStore.set
 </script>
 
 <template>
-  <RouterLink :to="to" class="h-[var(--g-sidebar-logo-height)] w-inherit flex-center gap-2 px-3 text-inherit no-underline" :class="{ 'cursor-pointer': settingsStore.settings.home.enable }" :title="title">
+  <RouterLink :to class="h-[var(--g-sidebar-logo-height)] w-inherit flex-center gap-2 px-3 text-inherit no-underline" :class="{ 'cursor-default': !settingsStore.settings.home.enable }" :title="title">
     <img v-if="showLogo" :src="logo" class="logo h-[30px] w-[30px] object-contain">
     <span v-if="showTitle" class="block truncate font-bold">{{ title }}</span>
   </RouterLink>
