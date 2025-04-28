d7808da1ca27f5f46117943df27ffbd0779ef894
Hooray <304327508@qq.com>
2025-04-28 18:21:01 +0800

    feat: 新增预设插槽 `header-after-menu`、`main-sidebar-after-menu` 和 `sub-sidebar-after-menu`

--- src/layouts/components/SubSidebar/index.vue
+++ src/layouts/components/SubSidebar/index.vue
@@ -74,6 +74,7 @@ watch(() => menuStore.actived, (val, oldVal) => {
           <FaIcon name="toolbar-collapse" class="size-4" />
         </FaButton>
       </div>
+      <component :is="useSlots('sub-sidebar-after-menu')" />
       <div v-if="settingsStore.settings.menu.mode === 'single'" class="flex-center px-4 pb-3">
         <AccountButton :only-avatar="settingsStore.settings.menu.subMenuCollapse" dropdown-align="center" :dropdown-side="settingsStore.settings.menu.subMenuCollapse ? 'right' : 'top'" button-variant="secondary" :class="{ 'w-full': !settingsStore.settings.menu.subMenuCollapse }" />
       </div>
