e086017689117156470def386ca0acfbb9818315
Hooray <304327508@qq.com>
2025-07-30 23:52:05 +0800

    refactor: 优化菜单项初始化逻辑

--- src/layouts/components/Menu/index.vue
+++ src/layouts/components/Menu/index.vue
@@ -43,7 +43,7 @@ const isMenuPopup = computed<MenuInjection['isMenuPopup']>(() => {
 function initItems(menu: MenuProps['menu'], parentPaths: string[] = []) {
   menu.forEach((item) => {
     const index = item.path ?? getUseId(item)
-    if (item.children) {
+    if (item.children?.some(item => item.meta?.menu !== false)) {
       const indexPath = [...parentPaths, index]
       subMenus.value[index] = {
         index,
