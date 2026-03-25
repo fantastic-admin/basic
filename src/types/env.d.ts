f343f63840b6c4e2c148ba83b3e2d63a73324f62
Hooray <304327508@qq.com>
2026-03-25 22:09:46 +0800

    更新依赖

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,14 +1,6 @@
 interface ImportMetaEnv {
   // Auto generate by env-parse
   /**
-   * 应用配置面板
-   */
-  readonly VITE_APP_SETTING: boolean
-  /**
-   * 页面标题
-   */
-  readonly VITE_APP_TITLE: string
-  /**
    * 接口请求地址，会设置到 axios 的 baseURL 参数上
    */
   readonly VITE_APP_API_BASEURL: string
@@ -21,12 +13,20 @@ interface ImportMetaEnv {
    */
   readonly VITE_APP_DISABLE_DEVTOOL: boolean
   /**
-   * 是否开启代理
+   * 应用配置面板
    */
-  readonly VITE_OPEN_PROXY: boolean
+  readonly VITE_APP_SETTING: boolean
+  /**
+   * 页面标题
+   */
+  readonly VITE_APP_TITLE: string
   /**
    * 是否开启开发者工具
    */
   readonly VITE_OPEN_DEVTOOLS: boolean
+  /**
+   * 是否开启代理
+   */
+  readonly VITE_OPEN_PROXY: boolean
   readonly VITE_VUE_DEVTOOLS_LAUNCH_EDITOR: string
 }
